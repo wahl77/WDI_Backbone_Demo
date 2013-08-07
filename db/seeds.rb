a559625d0b14decb4989bba5de5d9485117efcf8
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-08-07 14:22:55 -0700

    Made seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Question.create!(content: "How many minutes from sun to earth at lightspeed?")
+Question.create!(content: "What is your name?")
+Question.create!(content: "What is the capital of France?")
+Question.create!(content: "Who shot Alexander Hamilton?")
