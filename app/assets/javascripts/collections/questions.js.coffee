1428f659d2c2e27c2d38800b60fe360c6d7cfd8c
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-08-07 13:59:22 -0700

    Start of the API in rails

--- app/assets/javascripts/collections/questions.js.coffee
+++ app/assets/javascripts/collections/questions.js.coffee
@@ -1,3 +1,4 @@
 class WdiBackboneDemo.Collections.Questions extends Backbone.Collection
+  url: '/api/questions' # This will by default have all RESTful Routings. They are mirrored from the rails routes (nice default!) 
 
-  model: WdiBackboneDemo.Models.Question
+  #model: WdiBackboneDemo.Models.Question
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+  
+  respond_to :json
+
+  def index
+    respond_with Question.all
+  end
+
+  def show
+    respond_with Question.find(parmas[:id])
+  end
+
+  def create
+    respond_with Question.create(params[:question])
+  end
+
+  def update
+    respond_with Question.update(params[:id], params[:question])
+  end
+
+  def destroy
+    respond_with Question.destroy(parmas[:id])
+  end
+
 end
